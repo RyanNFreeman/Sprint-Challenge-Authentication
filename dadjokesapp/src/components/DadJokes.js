d654e53877ce1a0acd1b0460d2059eb193ca7b25
RyanNFreeman <ryan.norman.freeman@gmail.com>
2019-04-05 11:52:16 -0600

    get request not quite working from axios. Working on it now.

--- dadjokesapp/src/components/DadJokes.js
+++ dadjokesapp/src/components/DadJokes.js
@@ -1,7 +1,39 @@
 import React from "react";
+import axios from "axios";
 
-const DadJokes = () => {
-  return <h2>These are Dad Jokes</h2>; //needs logic. If token show dad jokes. if !token you must log in redirect
-};
+class DadJokes extends React.Component {
+  state = {
+    jokes: []
+  };
+  render() {
+    return (
+      <div>
+        <h2>These are Dad Jokes</h2>
+        <ul>
+          {this.state.jokes.map(joke => (
+            <li key={joke.id}>{joke.joke}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  } //needs logic. If token show dad jokes. if !token you must log in redirect
+  componentDidMount() {
+    const endpoint = "http://localhost:3300/api/jokes";
+    const token = localStorage.getItem("token");
+    const requestOptions = {
+      headers: {
+        authorization: token
+      }
+    };
+    axios
+      .get(endpoint, requestOptions)
+      .then(res => {
+        this.setState({ jokes: res.data });
+      })
+      .catch(err => {
+        console.error("Big error", err);
+      });
+  }
+}
 
 export default DadJokes;

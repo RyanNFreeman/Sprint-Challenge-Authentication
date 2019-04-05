2f01bbec1f20521e2013bfc2554619d0467f5206
RyanNFreeman <ryan.norman.freeman@gmail.com>
2019-04-05 09:53:28 -0600

    token now working. Going to abstract to middleware folder.

--- config/routes.js
+++ config/routes.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const jwt = require("jsonwebtoken");
 
 const axios = require("axios");
 const bcrypt = require("bcryptjs");
@@ -20,7 +21,7 @@ function register(req, res) {
 
   let user = req.body;
   const hash = bcrypt.hashSync(user.password, 8);
-  password = hash;
+  user.password = hash;
 
   Users.add(user)
     .then(created => {
@@ -33,6 +34,36 @@ function register(req, res) {
 
 function login(req, res) {
   // implement user login
+
+  let { username, password } = req.body;
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+        res.status(200).json({ message: `Welcome ${user.username}!`, token });
+      } else {
+        res
+          .status(401)
+          .json({ message: "You did not provide the correct credentials" });
+      }
+    })
+    .catch(error =>
+      res.status(500).json({ error: `there has been an error ${error}` })
+    );
+}
+
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username
+  };
+  const options = {
+    expiresIn: "1d"
+  };
+
+  return jwt.sign(payload, secret, options);
 }
 
 function getJokes(req, res) {
